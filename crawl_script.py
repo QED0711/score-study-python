b46cf35155567ded68e9c865fad3e9ca8e1c3e59
Quinn Dizon <qdizon@gmail.com>
2019-09-29 19:15:07 -0400

    added continuous crawl in crawl_script

--- crawl_script.py
+++ crawl_script.py
@@ -9,8 +9,9 @@ from work_crawler import get_work_pages, get_score_links, get_all_pdf_links, sav
 from mlab_db import get_composers
 
 if __name__ == '__main__':
-    composers = get_composers()
-    urls = [c['composer_url'] for c in composers if not c.get('visited')] 
-    for url in urls:
-        cp = CrawlPipeline(url)
-        cp.get_work_pdfs(max_scores=None) # set max_scores to set limit
+    while True: # continuous crawl
+        composers = get_composers()
+        urls = [c['composer_url'] for c in composers if not c.get('visited')] 
+        for url in urls:
+            cp = CrawlPipeline(url)
+            cp.get_work_pdfs(max_scores=None) # set max_scores to set limit
